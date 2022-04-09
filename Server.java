cee64cc5fa887e23195a6e7d1dad6e9114628573
Miltos <miltoskal11@protonmail.com>
2022-04-09 21:05:07 +0300

    Test

--- Server.java
+++ Server.java
@@ -1,3 +1,33 @@
-public class Server {
+import java.io.*;
+import java.net.*;
 
+public class Server {
+    public static void main(String args[]) {
+        new Server().openServer();
+    }
+
+    ServerSocket providerSocket;
+    Socket connection = null;
+
+    void openServer() {
+        try {
+            providerSocket = new ServerSocket(4321, 10);
+
+            while (true) {
+                connection = providerSocket.accept();
+
+                Thread t = new ActionsForClients(connection);
+                t.start();
+
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        } finally {
+            try {
+                providerSocket.close();
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
 }
